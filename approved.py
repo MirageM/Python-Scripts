ffa6f12bc61a362056f0efbde46c530d2043a7fe
MirageM <imiragesz@gmail.com>
2023-04-18 22:33:53 -0400

    Approved Message Check

---
+++ approved.py
@@ -0,0 +1,6 @@
+my_string = "This request has been Approved"
+if my_string is not None and "Approved" in my_string:
+    print("The word 'Approved' exist in the string")
+else:
+    print("The word 'Approved' does not exist in the string")
+    
