3aaccc9c62ab0ec8a0df6c1eb65de614abefdf62
MirageM <imiragesz@gmail.com>
2023-04-18 22:40:46 -0400

    Data Frame group by column & dropping duplicates

--- automation.py
+++ automation.py
@@ -1,8 +1,13 @@
-x = 10
-y = 'Hi'
-z = 'Hello'
-print(y)
+import pandas as pd
 
-breakpoint()
+# Read in Excel file
+df = pd.read_excel('my_excel_file.xlsx')
 
-print(z)
+# Group by Requirement column and sum other columns
+df = df.groupby('Requirement').sum().reset_index()
+
+# Drop duplicates in Requirement column
+df.drop_duplicates(subset='Requirement', keep = 'first', inplace = True)
+
+# Print resulting dataframe
+print(df)
